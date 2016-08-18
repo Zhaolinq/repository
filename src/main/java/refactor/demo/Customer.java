c1e1af1133cf16f415a1d3ac641aeef5455055dc
zhaolinqing <zhaolinqing@corp.netease.com>
2016-08-18 11:38:26 +0800

    extract method, move method

--- src/main/java/refactor/demo/Customer.java
+++ src/main/java/refactor/demo/Customer.java
@@ -28,25 +28,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = rentals.nextElement();
             
-            switch(each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2; 
-                if(each.getDayRented() > 2) {
-                    thisAmount += (each.getDayRented() - 2) * 1.5;
-                }
-                break;
-                
-            case Movie.NEW_RELEASE :
-                thisAmount += each.getDayRented() * 3;
-                break;
-                
-            case Movie.CHILDRENS :
-                thisAmount += 1.5;
-                if(each.getDayRented() > 3) {
-                    thisAmount += (each.getDayRented() - 3) * 1.5;
-                }
-                break;
-            }
+            thisAmount = each.getCharge();
             
             frequestRenterPoints ++;
             
--- src/main/java/refactor/demo/Rental.java
+++ src/main/java/refactor/demo/Rental.java
@@ -17,4 +17,27 @@ public class Rental {
     public void setDayRented(int dayRented) {
         this.dayRented = dayRented;
     }
+    double getCharge() {
+        double thisAmount = 0;
+        switch(getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2; 
+            if(getDayRented() > 2) {
+                thisAmount += (getDayRented() - 2) * 1.5;
+            }
+            break;
+            
+        case Movie.NEW_RELEASE :
+            thisAmount += getDayRented() * 3;
+            break;
+            
+        case Movie.CHILDRENS :
+            thisAmount += 1.5;
+            if(getDayRented() > 3) {
+                thisAmount += (getDayRented() - 3) * 1.5;
+            }
+            break;
+        }
+        return thisAmount;
+    }
 }
